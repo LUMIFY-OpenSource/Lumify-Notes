7234829d014b68cd845d7d2df0fb5808517d0ad5
Shatanik Mahanty <shatanikmahanty@gmail.com>
2024-07-10 15:03:06 +0530

    WIP add notebook UI:
    - Add Emoji picker
    - Add Color picker

--- lib/configurations/constants/constants.dart
+++ lib/configurations/constants/constants.dart
@@ -2,6 +2,8 @@
 
 // import 'package:lumify_notes/configurations/constants/fonts.gen.dart';
 
+import 'package:flutter/material.dart';
+
 ///Generate by running ```fluttergen``` in the terminal
 // export 'assets.gen.dart';
 
@@ -11,3 +13,9 @@ const kAppBarTitle = 'Lumify Notes';
 //TODO: Add app's font family
 const kBrandFontFamily = "FontFamily.quando";
 const kFontFamily = "FontFamily.roboto";
+
+const notebookPredefinedColors = [
+  Colors.orange,
+  Colors.green,
+  Color(0xFFE57373),
+];
--- lib/features/notebook/presentation/create_notebook_dialog_box.dart
+++ lib/features/notebook/presentation/create_notebook_dialog_box.dart
@@ -1,9 +1,18 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:lumify_notes/configurations/configurations.dart';
 
-import '../../../configurations/theme/size_constants.dart';
-
-class CreateNotebookDialogBox extends StatelessWidget {
+class CreateNotebookDialogBox extends StatefulWidget {
   const CreateNotebookDialogBox({super.key});
+
+  @override
+  State<CreateNotebookDialogBox> createState() => _CreateNotebookDialogBoxState();
+}
+
+class _CreateNotebookDialogBoxState extends State<CreateNotebookDialogBox> {
+  String? pickedEmoji;
+  Color? pickedColor;
 
   @override
   Widget build(BuildContext context) {
@@ -16,60 +25,136 @@ class CreateNotebookDialogBox extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Notebook Name",
-                style: theme.textTheme.labelMedium
-                    ?.copyWith(color: theme.colorScheme.primary),
+              alignment: Alignment.topRight,
+              child: GestureDetector(
+                child: const Icon(
+                  Icons.cancel_outlined,
+                  //TODO: Add color
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
               ),
+            ),
+            Text(
+              "Notebook Name",
+              style: theme.textTheme.labelMedium?.copyWith(color: theme.colorScheme.primary),
             ),
             Padding(
               padding: const EdgeInsets.all(kPadding),
               child: TextField(
                 decoration: InputDecoration(
                   hintText: "Enter notebook name",
-                  hintStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.outline,
-                      fontWeight: FontWeight.w400),
+                  hintStyle: TextStyle(color: Theme.of(context).colorScheme.outline, fontWeight: FontWeight.w400),
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
+            const SizedBox(height: kPadding),
+            Row(children: [
+              Text(
                 "EmojiTag",
-                style: theme.textTheme.labelMedium
-                    ?.copyWith(color: theme.colorScheme.primary),
+                style: theme.textTheme.labelMedium?.copyWith(color: theme.colorScheme.primary),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () async {
+                  final pickedEmoji = await showDialog(context: context, builder: (_) => const _EmojiPickerDialog());
+
+                  if (pickedEmoji != null) {
+                    setState(() {
+                      this.pickedEmoji = pickedEmoji;
+                    });
+                  }
+                },
+                child: Text(pickedEmoji ?? '🙂'),
+              ),
+            ]),
+            const SizedBox(height: kPadding),
+            Text(
+              "Select color",
+              style: theme.textTheme.labelMedium?.copyWith(color: theme.colorScheme.primary),
+            ),
+            SizedBox(
+              height: kPadding * 5,
+              child: ListView.builder(
+                itemCount: notebookPredefinedColors.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final color = notebookPredefinedColors[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          pickedColor = color;
+                        });
+                      },
+                      child: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: color,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: pickedColor == color ? Colors.black : Colors.transparent,
+                            width: 2,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-            // TODO: Add emoji picker
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Select color",
-                style: theme.textTheme.labelMedium
-                    ?.copyWith(color: theme.colorScheme.primary),
-              ),
-            ),
-            // TODO: Add color picker
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(kPadding),
               child: GestureDetector(
                 onTap: () {},
                 child: Align(
                   alignment: Alignment.bottomRight,
                   child: Text(
                     "Save",
-                    style: theme.textTheme.bodyMedium
-                        ?.copyWith(color: theme.colorScheme.primary),
+                    style: theme.textTheme.bodyMedium?.copyWith(color: theme.colorScheme.primary),
                   ),
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _EmojiPickerDialog extends StatelessWidget {
+  const _EmojiPickerDialog();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          EmojiPicker(
+            onEmojiSelected: (category, emoji) {
+              Navigator.pop(context, emoji.emoji);
+            },
+            config: Config(
+              checkPlatformCompatibility: true,
+              emojiViewConfig: EmojiViewConfig(
+                emojiSizeMax: 28 * (defaultTargetPlatform == TargetPlatform.iOS ? 1.20 : 0.9),
+              ),
+              swapCategoryAndBottomBar: true,
+              skinToneConfig: const SkinToneConfig(),
+              categoryViewConfig: const CategoryViewConfig(),
+              bottomActionBarConfig: const BottomActionBarConfig(),
+            ),
+          ),
+        ],
       ),
     );
   }
