3b8c7e6edf4e7f2a9dd87e96a06bc9654f29cb7d
mousumi <moumipal2002@gmail.com>
2024-10-03 12:45:16 +0530

    Update: Notebook model

--- lib/features/notebook/models/notebook_model.dart
+++ lib/features/notebook/models/notebook_model.dart
@@ -1,14 +1,20 @@
-
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'notebook_model.freezed.dart';
+
+part 'notebook_model.g.dart';
+
 @freezed
-class NoteBook with _$NoteBook{
+class NoteBook with _$NoteBook {
   const factory NoteBook({
     @JsonKey(name: '_id') required String id,
     required String name,
-    @JsonKey(includeIfNull: false) String? email,
-    @JsonKey(includeIfNull: false) String? profilePicture,
+    required String color,
+    @JsonKey(includeIfNull: false) DateTime? createdAt,
+    @JsonKey(includeIfNull: false) DateTime? updatedAt,
+    @JsonKey(includeIfNull: false) String? emoji,
   }) = _NoteBook;
+
+  factory NoteBook.fromJson(Map<String, dynamic> json) =>
+      _$NoteBookFromJson(json);
 }
-factory NoteBook.fromJson(Map<String, dynamic> json) =>
-_$NoteBookFromJson(json);
